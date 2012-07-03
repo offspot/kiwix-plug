dbba30d14e3d7708968d7e3c3419c1c02b9c5e67
kelson42 <kelson42@users.sourceforge.net>
2012-07-03 15:38:28 +0000

    + improvements

--- setup_usbkey.sh
+++ setup_usbkey.sh
@@ -23,10 +23,34 @@ else
     ROOT=`cd $ROOT ; pwd`
 fi
 
+# Check if the mtools are installed
+MLABEL=`whereis mlabel | cut --delimiter=":" -f2 | cut --delimiter=" " -f2`
+if [ "$MLABEL" = "" ]
+then
+    echo "You need to install the mtools (apt-get install mtools)."
+    exit 1
+fi
+
 # Detect USB storage
-for DEVICE in `df | sed "s/^ *//;s/ *$//;s/ \{1,\}/ /g" | cut --delimiter=" " -f6 | grep "/media/"`
+for MOUNT in `df | sed "s/^ *//;s/ *$//;s/ \{1,\}/ /g" | cut --delimiter=" " -f6 | grep "/media/"`
 do
-    echo $DEVICE
+    if [ "$(ls -A "$MOUNT")" = "" ]
+    then
+	echo "Empty removable device found at $MOUNT. This will be used to install kiwix-plug."
+	DEVICE=`df | sed "s/^ *//;s/ *$//;s/ \{1,\}/ /g" | grep "$MOUNT" | cut --delimiter=" " -f1`
+	break
+    fi
 done
 
+# Check if an empty removable device was found
+if [ "$MOUNT" = "" ]
+then
+    echo "Unable to find an empty removable device. Are you sure you have put a USB key to your computer?"
+fi
+
 # Set USB label
+echo "Setting new label KIWIX to USB key at $DEVICE ..."
+echo "drive a: file=\"$DEVICE\"" > ~/.mtoolsrc
+sudo mlabel a:42
+sudo mlabel a:KIWIX
+sudo mlabel -s a:
