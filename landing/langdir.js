4aed45b037af65a44b1ce40ac1aad54821f108a4
kelson42 <kelson42@users.sourceforge.net>
2012-08-03 17:12:21 +0000

    + small fix in the language detection

--- landing/langdir.js
+++ landing/langdir.js
@@ -13,7 +13,7 @@ lang = lang.substr(0,2);
 var dest = window.location.href;
 for (i=langcodes.length-1;i >= 0; i--) {
     if (lang==langcodes[i]) {
-	dest = dest.substr(0,dest.lastIndexOf('.')) + '-' + lang.substr(0,2) + dest.substr(dest.lastIndexOf('.'));
+	dest = '/index-' + lang.substr(0,2) + '.html';
 	window.location.replace ? window.location.replace(dest) : window.location=dest;
     }
 }
